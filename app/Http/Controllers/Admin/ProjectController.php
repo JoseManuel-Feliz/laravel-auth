301ee6f281dc955f0c952e37e0c50cec03888658
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-11-05 23:03:52 +0100

    fix: rename ProjectModel class to Project. added: index method to project controller

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Project;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -12,7 +13,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        $projects = Project::all();
+        return view('index', compact('projects'));
     }
 
     /**
