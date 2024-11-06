71a80cf32a0a6fdfdd6b7382cdafe7f86796388e
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-11-06 21:41:34 +0100

    added: table, create, show, edit and delete btns

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -5,18 +5,52 @@
 @section('nav-title','Projects')
 
 @section('main-content')
+<div class="container">
 
-@forelse($projects as $project)
+    <div><a class="btn btn-success" href="#">create</a></div>
 
-<p>{{$project->title}}</p>
-<p>{{$project->status}}</p>
-<p>{{$project->author}}</p>
-<p>{{$project->contributors}}</p>
-<p>{{$project->project_start_date}}</p>
-<p>{{$project->description}}</p>
 
-@empty
+    <table class="text-center">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Status</th>
+                <th>Author</th>
+                <th>Contributors</th>
+                <th>Project date</th>
+                <th>Description</th>
+                <th colspan="3">Btns</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
 
-@endforelse
+                @forelse($projects as $project)
 
+                <td>{{$project->title}}</td>
+                <td>{{$project->status}}</td>
+                <td>{{$project->author}}</td>
+                <td>{{$project->contributors}}</td>
+                <td>{{$project->project_start_date}}</td>
+                <td>{{Str::words($project->description,3,'...')}}</td>
+                <td><a class="btn btn-secondary" href="#">show</a></td>
+                <td><a class="btn btn-warning" href="#">edit</a></td>
+                <td>
+                    <form action="#" method="POST">
+                        @csrf
+                        {{method_field('DELETE')}}
+                        <button class="btn btn-danger" type="submit">delete</button>
+                    </form>
+                </td>
+            </tr>
+
+            @empty
+            <tr>
+                <td colspan="7">There aren't projects</td>
+            </tr>
+            @endforelse
+        </tbody>
+    </table>
+
+</div>
 @endsection
