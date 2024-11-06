089e23ae7b8390f4baffec02b76b1254a11773c2
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-11-06 22:29:25 +0100

    add: create edit-create form layout

---
+++ resources/views/layouts/create-edit.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('page-title','Create')
+
+@section('nav-title', 'Create')
+
+@section('main-content')
+
+<form action="@yield('form-route')" method="POST">
+    @csrf
+
+    @yield('form-method','')
+
+    <div>
+        <label for="title">Project title</label>
+        <input type="text" name="title" id="title" value="{{old('title',$project->title)}}">
+    </div>
+    <div>
+        <label for="status">Project status</label>
+        <input type="text" name="status" id="status" value="{{old('status',$project->status)}}">
+    </div>
+    <div>
+        <label for="author">Author</label>
+        <input type="text" name="author" id="author" value="{{old('author',$project->author)}}">
+    </div>
+    <div>
+        <label for="contributors">contributors</label>
+        <input type="text" name="contributors" id="contributors" value="{{old('contributors',$project->contributors)}}">
+    </div>
+    <div>
+        <label for="project-date">Project date</label>
+        <input type="text" name="project_start_date" id="project-date"
+            value="{{old('project_start_date',$project->project_start_date)}}">
+    </div>
+    <div>
+        <label for="description">Description</label>
+        <input type="text" name="description" id="description" value="{{old('description',$project->description)}}">
+    </div>
+
+    @yield('additionals-inputs','')
+
+    <div>
+        <button type="submit">@yield('submit-btn-text')</button>
+        <button type="reset">@yield('reset-btn-text')</button>
+    </div>
+</form>
+
+
+
+@endsection
