b23421dc32597cab09b8411473495a6825e3f448
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-11-06 09:31:36 +0100

    fix: contributors fake data on seed. varchar lenght on migration contributors column

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -16,16 +16,23 @@ class ProjectSeeder extends Seeder
     {
         for ($i = 0; $i < 300; $i++) {
 
-            $ramdonTotalContributors = rand(2, 5);
             $newProject = new Project();
             $newProject->title = $faker->realTextBetween(2, 15);
             $newProject->author = $faker->firstName() . ' ' . $faker->lastName();
-            for ($j = 0; $j < $ramdonTotalContributors; $j++) {
-                $newProject->contributors = $faker->firstName() . ' ' . $faker->lastName();
-            };
             $newProject->status = $faker->boolean();
             $newProject->Project_start_date = $faker->dateTimeBetween('-5 weeks', 'now');
             $newProject->description = $faker->realTextBetween(60, 300);
+
+
+
+            $ramdonTotalContributors = rand(2, 5);
+            $contributors = [];
+            for ($j = 0; $j < $ramdonTotalContributors; $j++) {
+                $contributors[] = $faker->firstName() . ' ' . $faker->lastName();
+            };
+
+            $newProject->contributors = implode(', ', $contributors);
+
             $newProject->save();
         }
     }
