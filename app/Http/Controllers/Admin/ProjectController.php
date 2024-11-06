6a2042a63911a3b4985f40646a1d5205245e34f4
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-11-06 23:05:02 +0100

    fix: create methods in admin proctect controller

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -21,16 +21,16 @@ class ProjectController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create() {}
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function create()
     {
         $project = new Project();
         return view('Admin.projects.create', compact('project'));
     }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request) {}
 
     /**
      * Display the specified resource.
