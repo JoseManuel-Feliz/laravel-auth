2f1fc3c9c75aae5b12c9500636eb9f75f6b139a8
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-11-11 08:54:28 +0100

    feat: removed mockup data on edit create layouts

--- resources/views/layouts/create-edit.blade.php
+++ resources/views/layouts/create-edit.blade.php
@@ -2,63 +2,73 @@
 
 @section('main-content')
 
+<div class="container">
 
-<form action="@yield('form-route')" method="POST">
-    @csrf
 
-    @yield('form-method','')
+    <form action="@yield('form-route')" method="POST">
+        @csrf
 
-    <div>
-        <label for="title">Project title</label>
-        <input type="text" name="title" id="title" value="{{old('title',$project->title)}}">
-        @error('title')
-        <div>{{$message}}</div>
-        @enderror
-    </div>
-    <div>
-        <label for="status">Project status</label>
-        <input type="text" name="status" id="status" value="{{old('status',$project->status)}}">
-        @error('status')
-        <div>{{$message}}</div>
-        @enderror
-    </div>
-    <div>
-        <label for="author">Author</label>
-        <input type="text" name="author" id="author" value="{{old('author',$project->author)}}">
-        @error('author')
-        <div>{{$message}}</div>
-        @enderror
-    </div>
-    <div>
-        <label for="contributors">contributors</label>
-        <input type="text" name="contributors" id="contributors" value="{{old('contributors',$project->contributors)}}">
-        @error('contributors')
-        <div>{{$message}}</div>
-        @enderror
-    </div>
-    <div>
-        <label for="project-date">Project date</label>
-        <input type="text" name="project_start_date" id="project-date"
-            value="{{old('project_start_date',$project->project_start_date)}}">
-        @error('project_start_date')
-        <div>{{$message}}</div>
-        @enderror
-    </div>
-    <div>
-        <label for="description">Description</label>
-        <input type="text" name="description" id="description" value="{{old('description',$project->description)}}">
-        @error('description')
-        <div>{{$message}}</div>
-        @enderror
-    </div>
+        @yield('form-method','')
+        <div>
+            <label for="title">Project title</label>
+            <input type="text" name="project_title" id="title" value="{{old('project_title',$project->project_title)}}">
+            @error('project_title')
+            <div>{{$message}}</div>
+            @enderror
+        </div>
+        <div>
+            <label for="project-date">Launch date</label>
+            <input type="text" name="launch_date" id="project-date"
+                value="{{old('launch_date',$project->launch_date)}}">
+            @error('launch_date')
+            <div>{{$message}}</div>
+            @enderror
+        </div>
+        <div>
+            <label for="status">Project status</label>
+            <input type="text" name="project_status" id="status"
+                value="{{old('project_status',$project->project_status)}}">
+            @error('project_status')
+            <div>{{$message}}</div>
+            @enderror
+        </div>
+        <div>
+            <label for="repo-link">Repository url</label>
+            <input type="text" name="repository_url" id="repo-link"
+                value="{{old('repository_url',$project->repository_url)}}">
+            @error('repository_url')
+            <div>{{$message}}</div>
+            @enderror
+        </div>
+        <div>
+            <label for="img-url">Project thumbnail</label>
+            <input type="text" name="project_thumbnail" id="img-url"
+                value="{{old('project_thumbnail',$project->project_thumbnail)}}">
+            @error('project_thumbnail')
+            <div>{{$message}}</div>
+            @enderror
+        </div>
+        <td>{{Str::words($project->project_summary,5,'...')}}</td>
+        <div>
+            <label for="description">Description</label>
+            <input type="text" name="description" id="description"
+                value="{{old('project_summary',$project->project_summary)}}">
+            @error('description')
+            <div>{{$message}}</div>
+            @enderror
+        </div>
 
-    @yield('additionals-inputs','')
+        @yield('additionals-inputs','')
 
-    <div>
-        <button type="submit">@yield('submit-btn-text', '')</button>
-        <button type="reset">@yield('reset-btn-text', '')</button>
-    </div>
-</form>
+        <div>
+            <button type="submit">@yield('submit-btn-text', '')</button>
+            <button type="reset">@yield('reset-btn-text', '')</button>
+        </div>
+    </form>
+
+</div>
+
+
 
 
 
