a8fc4a997d3a977d5fea377062d2c99d44525805
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-11-11 08:46:26 +0100

    fix

--- database/migrations/2024_11_10_183226_update_remove_column_projects_table.php
+++ database/migrations/2024_11_10_183226_update_remove_column_projects_table.php
@@ -23,8 +23,8 @@ return new class extends Migration
     {
         Schema::table('projects', function (Blueprint $table) {
             $table->string('title', 40)->nullable(false);
-            $table->string('author', 30)->nullable(false)->after('title');
-            $table->string('contributors', 150)->nullable(false)->after('author');
+            $table->string('author', 30)->nullable(false);
+            $table->string('contributors', 150)->nullable(false);
             $table->date('project_start_date')->nullable(false);
             $table->boolean('status')->default(0)->nullable(false);
             $table->text('description')->nullable(true);
